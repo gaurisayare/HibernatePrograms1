26b1ab86b8072143580d81d8c333bac4274edeb3
gaurisayare <144650477+gaurisayare@users.noreply.github.com>
2023-10-12 11:55:16 +0530

    Add files via upload

---
+++ Hibernet1to1/src/main/java/com/gauri/example/Hibernate1to1/App.java
@@ -0,0 +1,40 @@
+package com.gauri.example.Hibernate1to1;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import com.gauri.example.Hibernate1to1.Entity.*;
+
+
+public class App 
+{
+    public static void main(String[] args) 
+    {
+    	SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		try 
+		{
+		  User user=new User();
+		  user.setUsername("soma");
+		  
+		  Address address=new Address();
+		  address.setStreet("Airoli");
+		  
+		  user.setAddress(address);
+		  address.setUser(user);
+		  
+		  session.save(user);
+		  session.getTransaction().commit();
+		
+		  User retrievedUser=session.get(User.class, user.getId());
+		  Address retrievedAddress=retrievedUser.getAddress();
+		  
+		  System.out.println("User: "+retrievedUser.getUsername());
+		  System.out.println("Address: "+retrievedAddress.getStreet());
+		}
+		finally 
+		{
+			session.close();
+            sessionFactory.close();
+		}
+	}
+}
